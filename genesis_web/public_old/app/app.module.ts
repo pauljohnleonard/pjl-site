7c0deac8ac0547b1d3ca7bc2730731bf094823e8
pauljohnleonard <pauljohnleonard@gmail.com>
2017-02-09 18:53:30 +0000

    clean up

--- genesis_web/public_old/app/app.module.ts
+++ genesis_web/public_old/app/app.module.ts
@@ -6,6 +6,7 @@ import { MaterialModule } from '@angular/material';
 
 import { AppComponent }   from './app.component';
 
+/*
 import { AIDetailComponent } from './music/ai-detail.component';
 import { PlayerDetailComponent } from './music/player-detail.component';
 import { InstrumentDetailComponent } from './music/instrument-detail.component';
@@ -14,17 +15,16 @@ import { MusicComponent } from './music/music.component';
 import { MusicAppComponent } from './music-app.component';
 import { DBService } from './services/db.service';
 import { SFService } from './services/sf.service';
-//import 'hammerjs'; // Recommended
-
+import '../node_modules/hammerjs/hammer'; // Recommended
+*/
 
 
 @NgModule({
   imports: [ BrowserModule,HttpModule,JsonpModule,FormsModule,
     MaterialModule.forRoot()  
   ],
-  declarations: [ AppComponent,AIDetailComponent,InstrumentDetailComponent,PlayerDetailComponent,
-        MusicComponent,MusicAppComponent],
-  providers: [DBService,SFService],
+  declarations: [ AppComponent],
+  providers: [],
   bootstrap:    [ AppComponent ]
 })
 
--- keep/devserver.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let express = require('express');
-let compression = require('compression');
-let app = express();
-app.use(compression());
-app.use('/', express.static('.'));
-app.listen(3000);
