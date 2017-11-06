151a68ae56d13d3d79d15de55af58eaa766177a3
Paul <pauljohnleonard@gmail.com>
2017-11-06 09:39:37 +0000

    maz midi parser

--- genesis_web/public/src/app/app.component.ts
+++ genesis_web/public/src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
 import { MatDialog, MatDialogConfig, MatDialogRef, MatSnackBar } from '@angular/material';
-import { DBService } from './services/db.service'
-import { MusicAppComponent } from './music-app.component'
+import { DBService } from './services/db.service';
+import { MusicAppComponent } from './music-app.component';
 
-import { LoadDialogComponent } from './load/load.dialog'
-import { MetroDialogComponent } from './metro/metro.dialog'
+import { LoadDialogComponent } from './load/load.dialog';
+import { MetroDialogComponent } from './metro/metro.dialog';
 
-import { SettingsService } from './services/settings.service'
-import { MidiParser } from './model/midiparser'
+import { SettingsService } from './services/settings.service';
+import { MidiParser } from './model/midiparser';
 
 // declare var JSMIDIParser;
 
-declare var MidiFile
+declare var MidiFile;
 
 @Component({
     moduleId: 'app/',
@@ -22,16 +22,16 @@ declare var MidiFile
 export class AppComponent implements OnInit {
 
 
-    @ViewChild(MusicAppComponent) musicApp: MusicAppComponent
-    @ViewChild('midiload') midiloader: ElementRef
-    user: any = null
+    @ViewChild(MusicAppComponent) musicApp: MusicAppComponent;
+    @ViewChild('midiload') midiloader: ElementRef;
+    user: any = null;
 
     constructor(private dbService: DBService, public dialog: MatDialog,
         public snackBar: MatSnackBar, public settings: SettingsService) {
         dbService.register((user: any) => {
-            this.user = user
+            this.user = user;
 
-        })
+        });
 
         //  this.visibility()
     }
@@ -40,37 +40,37 @@ export class AppComponent implements OnInit {
 
 
         this.midiloader.nativeElement.onchange = (InputEvt) => {
-            console.log(' on selected file ')
+            console.log(' on selected file ');
             if (!this.midiloader.nativeElement.files.length) { return false; }
-            const reader = new FileReader()
-            const file =	this.midiloader.nativeElement.files[0]
+            const reader = new FileReader();
+            const file =	this.midiloader.nativeElement.files[0];
             reader.readAsBinaryString(file);	// read the binary data
             reader.onload = ((t) => {			// when ready...
 
-                const midi = new MidiFile(reader.result)
+                const midi = new MidiFile(reader.result);
 
-                this.musicApp.newMusic()
-                const music = this.musicApp.music
-                MidiParser.loadMidi(midi, music)
-                let nn = file.name
+                this.musicApp.newMusic();
+                const music = this.musicApp.music;
+                MidiParser.loadMidi(midi, music);
+                let nn = file.name;
                 const ii = nn.lastIndexOf('/');
-                if (ii > 0) {nn = nn.substring(ii)}
-                music.title = nn
+                if (ii > 0) {nn = nn.substring(ii); }
+                music.title = nn;
 
             }
-            )
+            );
         };
     }
 
     signIn() {
-        this.dbService.signIn()
-        console.log('signing in')
+        this.dbService.signIn();
+        console.log('signing in');
     }
 
 
     signOut() {
-        this.dbService.signOut()
-        console.log('signing out')
+        this.dbService.signOut();
+        console.log('signing out');
     }
 
     save() {
@@ -78,12 +78,12 @@ export class AppComponent implements OnInit {
         if (this.musicApp.music.id !== null) {
             this.snackBar.open(' Already saved', this.musicApp.music.id, {
                 duration: 2000,
-            })
+            });
         } else {
-            this.musicApp.music.saveDB(this.dbService)
+            this.musicApp.music.saveDB(this.dbService);
             this.snackBar.open(' Saved ', this.musicApp.music.id, {
                 duration: 2000,
-            })
+            });
         }
 
     }
@@ -91,10 +91,10 @@ export class AppComponent implements OnInit {
     load() {
         const config = new MatDialogConfig();
 
-        const dialogRef: MatDialogRef<LoadDialogComponent> = this.dialog.open(LoadDialogComponent, config)
-        dialogRef.componentInstance.setUp(this.dbService, this.musicApp)
+        const dialogRef: MatDialogRef<LoadDialogComponent> = this.dialog.open(LoadDialogComponent, config);
+        dialogRef.componentInstance.setUp(this.dbService, this.musicApp);
         dialogRef.afterClosed().subscribe((result: any) => {
-            console.log(result)
+            console.log(result);
         });
 
     }
@@ -104,23 +104,23 @@ export class AppComponent implements OnInit {
     metroSetup() {
         const config = new MatDialogConfig();
 
-        const dialogRef: MatDialogRef<MetroDialogComponent> = this.dialog.open(MetroDialogComponent, config)
-        dialogRef.componentInstance.metro = this.musicApp.music.metro
+        const dialogRef: MatDialogRef<MetroDialogComponent> = this.dialog.open(MetroDialogComponent, config);
+        dialogRef.componentInstance.metro = this.musicApp.music.metro;
 
         dialogRef.afterClosed().subscribe((result: any) => {
-            console.log(result)
+            console.log(result);
         });
     }
 
     clean() {
-        this.dbService.clean()
+        this.dbService.clean();
         this.snackBar.open('Argghhhhhhh  ', 'OH NO!', {
             duration: 4000,
-        })
+        });
     }
 
     newMusic() {
-        this.musicApp.newMusic()
+        this.musicApp.newMusic();
     }
 
     /*
